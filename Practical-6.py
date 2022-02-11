41c28696b47ffeab847ccc175f1bc0c5ced2a5b2
PushtiPatel <97660590+PushtiPatel@users.noreply.github.com>
2022-02-11 11:01:53 +0530

    Add files via upload

--- Practical-6.py
+++ Practical-6.py
@@ -1,4 +1,5 @@
 # 20CE099-PUSHTI PATEl
+# AIM: You are given n words. Some words may repeat. For each word, output its number of occurrences. The output order should correspond with the input order of appearance of the word. See the sample input/output for clarification.
 from collections import Counter
 l=[]
 for i in range(int(input())):
